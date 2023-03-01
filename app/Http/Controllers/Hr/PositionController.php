58f360e0c2eae7a18d78f6a85b18641b2848c674
Mengtao Guo <guomengtao@gmail.com>
2023-03-01 15:52:15 +0800

    time

--- app/Http/Controllers/Hr/PositionController.php
+++ app/Http/Controllers/Hr/PositionController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Hr;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hr\Position;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+use App\Exceptions\InvalidOrderException;
 
 class PositionController extends Controller
 {
@@ -37,7 +42,14 @@ class PositionController extends Controller
      */
     public function show($id)
     {
-        //
+        // \Common::guid();
+        // 检索，如果重复，会抛 `MultipleRecordsFoundException` 异常，并断言重复的条数...
+        $result  =  Position::where(['id'  =>  $id])->sole();
+
+        $response['code'] = $result?'2':'200';
+        $response['msg']  = $result?'success':'数据不存在';
+        $response['data'] = $result ;
+        return json_encode($response);
     }
 
     /**
@@ -60,6 +72,16 @@ class PositionController extends Controller
      */
     public function destroy($id)
     {
-        //
+
+        // $result = Position::find($id);
+        // $result->deleted_at  = 'Paris to London';
+        // $result->save();
+        $result  =  Position::destroy($id);
+
+        $response['code'] = $result?'2':'200';
+        $response['msg']  = $result?'success':'数据不存在';
+        $response['data'] = $result ;
+        return json_encode($response);
+
     }
 }
